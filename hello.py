999f4d483b0dc9efde3df2fe409897ff5710706b
zzf <772091199@qq.com>
2020-06-13 10:43:16 +0800

    爬取代理IP

--- hello.py
+++ hello.py
@@ -1,1 +1,9 @@
-print('hello')
+import requests
+
+headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36'
+        }
+
+
+r=requests.get('https://www.xicidaili.com/nn/',headers=headers)
+print(r.text)
