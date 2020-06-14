217e129d7ac2374a7260e0cfc72212c64ee3cdb6
zzf <772091199@qq.com>
2020-06-14 11:24:13 +0800

    2020-6-14

---
+++ bilibili_up_top/bilibili_up_ranking_list.py
@@ -0,0 +1,48 @@
+import requests
+from lxml import etree
+import json
+
+def get_html_page(url):
+    try:
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36 Edg/83.0.478.45'
+        }
+        html = requests.get(url,headers=headers)
+        if html.status_code == 200:
+            return html.text
+        return None
+    except:
+        return None
+
+def parse_page(content):
+    html = etree.HTML(content)
+    result = html.xpath('//div[@class="ups-list"]/a')
+    for item in result:
+        name = (item.xpath('div//span[@class="name blue"]/text()'))[0]
+        index = (item.xpath('div//span[@class="index"]/text()'))[0]
+        face = (item.xpath('div//img[@class="face"]/@src'))[0]
+        sign = (item.xpath('div//div[@class="sign"]/text()'))[0]
+        fans = (item.xpath('div//p[contains(@class,"fans")]/text()'))[0] + html.xpath('//*[@id="app"]/div/div[3]/div[2]/a[1]/div/div[2]/div[2]/p[2]/text()')[0]
+        play_num = (item.xpath('div//p[contains(@class,"playNum")]/text()'))[0] + html.xpath('//*[@id="app"]/div/div[3]/div[2]/a[1]/div/div[2]/div[3]/p[2]/text()')[0]
+        total.append({
+            'index':index,
+            'name': name,
+            'face':face,
+            'sign':sign,
+            'fans':fans,
+            'play_num':play_num,
+        })
+
+def write_to_file(content):
+    with open('bilibili_up_ranking_list.txt','a',encoding='utf-8') as f:
+        f.write(json.dumps(content,ensure_ascii=False)+'\n')
+
+if __name__ == '__main__':
+    total = []
+    url = 'https://www.kanbilibili.com/rank/ups/fans'
+    html = get_html_page(url)
+    parse_page(html)
+    for i in total:
+        write_to_file(i)
+    print(total)
+
--- hello.py
+++ hello.py
@@ -1,72 +1,21 @@
-from bs4 import BeautifulSoup
+import requests
+from lxml import etree
+headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36 Edg/83.0.478.45'
+        }
+r = requests.get('https://www.kanbilibili.com/rank/ups/fans',headers=headers)
+html = etree.HTML(r.text)
+result = html.xpath('//div[@class="ups-list"]/a')
+# print(result)
+ls=[]
+for i in result:
+    s=(i.xpath('div//p[contains(@class,"playNum")]/text()'))[0] + html.xpath('//*[@id="app"]/div/div[3]/div[2]/a[1]/div/div[2]/div[3]/p[2]/text()')[0]
+    ls.append(s)
+print(ls)
+# result = html.xpath('//*[@id="app"]/div/div[3]/div[2]//div/div[1]/span[1]/text()')
+# print(result)
+# //*[@id="app"]/div/div[3]/div[2]/a[1]/div/div[1]/span[1]
+# //*[@id="app"]/div/div[3]/div[2]/a[2]/div/div[1]/span[1]
 
-text = '''
-  <div class="header">
-  <div class="header-inner">
-          <a href="//maoyan.com" class="logo" data-act="icon-click"></a>
-        <div class="city-container" data-val="{currentcityid:1035 }">
-            <div class="city-selected">
-                <div class="city-name">
-                  道县
-                  <span class="caret"></span>
-                </div>
-            </div>
-            <div class="city-list" data-val="{ localcityid: 1035 }">
-                <div class="city-list-header">定位城市：<a class="js-geo-city" data-ci="1035">道县</a></div>
-
-            </div>
-        </div>
 
 
-        <div class="nav">
-            <ul class="navbar">
-                <li><a href="/" data-act="home-click"  >首页</a></li>
-                <li><a href="/films" data-act="movies-click" >电影</a></li>
-                <li><a href="/cinemas" data-act="cinemas-click" >影院</a></li>
-                <li><a href="http://www.gewara.com">演出</a></li>
-
-                <li><a href="/board" data-act="board-click"  class="active" >榜单</a></li>
-                <li><a href="/news" data-act="hotNews-click" >热点</a></li>
-                <li><a href="/edimall"  >商城</a></li>
-            </ul>
-        </div>
-
-        <div class="user-info">
-            <div class="user-avatar J-login">
-              <img src="https://p0.meituan.net/movie/7dd82a16316ab32c8359debdb04396ef2897.png">
-              <span class="caret"></span>
-              <ul class="user-menu no-login-menu">
-                <li><a href="javascript:void 0">登录</a></li>
-              </ul>
-            </div>
-        </div>
-
-        <form action="/query" target="_blank" class="search-form" data-actform="search-click">
-            <input name="kw" class="search" type="search" maxlength="32" placeholder="找影视剧、影人、影院" autocomplete="off">
-            <input class="submit" type="submit" value="">
-        </form>
-
-        <div class="app-download">
-          <a href="/app" target="_blank">
-            <span class="iphone-icon"></span>
-            <span class="apptext">APP下载</span>
-            <span class="caret"></span>
-            <div class="download-icon">
-                <p class="down-title">扫码下载APP</p>
-                <p class='down-content'>选座更优惠</p>
-            </div>
-          </a>
-        </div>
-
-  </div>
-</div>
-'''
-import re
-soup = BeautifulSoup(text,'lxml')
-
-# result=soup.find(class_='navbar').find_all('li')
-print(soup.find('ul').li.a.string)
-# for i in result:
-#     print(i.find('a').string)
-# for i,child in enumerate(soup.ul.children):
-#     print(i,child)
