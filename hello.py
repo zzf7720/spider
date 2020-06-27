9acf6d8663616d6c5ce536520803616decc95b9c
zzf <772091199@qq.com>
2020-06-27 21:29:40 +0800

    删除venv

--- hello.py
+++ hello.py
@@ -1,20 +1,14 @@
-server = 'http://localhost:4723/wd/hub'
-desired_caps = {
-    "platformName": "Android",
-    "deviceName": "SM_G9860",
-    "appPackage": "com.tencent.mm",
-    "appActivity": ".ui.LauncherUI"
-}
+import os
+def clear(filepath):
+    files = os.listdir(filepath)
+    for fd in files:
+        cur_path = os.path.join(filepath, fd)
+        if os.path.isdir(cur_path):
+            if fd == "__pycache__":
+                print("rm %s -rf" % cur_path)
+                os.system("rm %s -rf" % cur_path)
+            else:
+                clear(cur_path)
 
-from appium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
-
-driver = webdriver.Remote(server,desired_caps)
-
-wait = WebDriverWait(driver,30)
-login = wait.until(EC.presence_of_element_located((By.ID,'com.tencent.mm:id/fam')))
-login.click()
-phone = wait.until(EC.presence_of_element_located((By.ID,'com.tencent.mm:id/bhn')))
-phone.send_keys("13047211898")
+if __name__ == "__main__":
+    clear("./")
